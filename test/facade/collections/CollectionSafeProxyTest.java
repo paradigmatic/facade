a470ab6769a12bfa680178579b78ae2022e642d7
JL Falcone <jean-luc.falcone@unige.ch>
2009-07-04 18:43:32 +0200

    Adding first unit tests class (but still empty).

---
+++ test/facade/collections/CollectionSafeProxyTest.java
@@ -0,0 +1,88 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package facade.collections;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author falcone
+ */
+public class CollectionSafeProxyTest {
+
+
+    @Before
+    public void setUp() {
+    }
+
+    /**
+     * Test of get method, of class CollectionSafeProxy.
+     */
+    @Test
+    public void testGet() {
+    }
+
+    /**
+     * Test of select method, of class CollectionSafeProxy.
+     */
+    @Test
+    public void testSelect() {
+    }
+
+    /**
+     * Test of reject method, of class CollectionSafeProxy.
+     */
+    @Test
+    public void testReject() {
+    }
+
+    /**
+     * Test of apply method, of class CollectionSafeProxy.
+     */
+    @Test
+    public void testApply() {
+    }
+
+    /**
+     * Test of add method, of class CollectionSafeProxy.
+     */
+    @Test
+    public void testAdd() {
+    }
+
+    /**
+     * Test of addAll method, of class CollectionSafeProxy.
+     */
+    @Test
+    public void testAddAll() {
+    }
+
+    /**
+     * Test of map method, of class CollectionSafeProxy.
+     */
+    @Test
+    public void testMap() {
+    }
+
+    /**
+     * Test of reduce method, of class CollectionSafeProxy.
+     */
+    @Test
+    public void testReduce() {
+    }
+
+    /**
+     * Test of fillWith method, of class CollectionSafeProxy.
+     */
+    @Test
+    public void testFillWith() {
+    }
+
+}
